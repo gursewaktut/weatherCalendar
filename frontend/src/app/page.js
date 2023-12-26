9dca232c0a962f9fa6425e7db2bbf965acef902c
Gursewak  Singh <gursewaktut@gmail.com>
2023-12-25 23:28:17 -0800

    fixing the calendar styling

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import styles from './page.module.css';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
+import { ChakraProvider, Grid, GridItem } from '@chakra-ui/react';
+import "react-calendar/dist/Calendar.css";
 
 
 //type ValuePiece = Date | null;
@@ -15,9 +17,34 @@ export default function Home() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <main className={styles.main}>
-      <h2>Wella</h2>
-      <Calendar onChange={onChange} value={value} />
-    </main>
+    <ChakraProvider>
+      <header>
+        <h1>Wella</h1>
+      </header>
+      <Grid templateColumns='repeat(5, 1fr)' gap={3}>
+        <GridItem colSpan={1}>
+        </GridItem>
+        <GridItem colSpan={3}>
+          <div display='flex' flexDirection='row' flexWrap='wrap' alignItems='flex-start' mx='10px' p='10px'>
+            <main border='1px' borderRadius='3px' display='flex' w='100%' flexBasis='420px' flexDirection='column' alignItems='stretch' pt='1em'>
+              <Calendar
+                mx='0'
+                my='auto'
+                border='1px'
+                borderStyle='solid'
+                borderRadius='3'
+                borderColor='red'
+                w='100%'
+                onChange={onChange}
+                showWeekNumbers
+                value={value}
+              />
+            </main>
+          </div>
+        </GridItem>
+        <GridItem colSpan={1}>
+        </GridItem>
+      </Grid>
+    </ChakraProvider>
   )
 }
