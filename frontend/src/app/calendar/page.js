ecfbfa6dc7a8ae3075adb44ae88329affba3a04b
Gursewak  Singh <gursewaktut@gmail.com>
2024-02-10 09:01:11 -0800

    added some css

--- frontend/src/app/calendar/page.js
+++ frontend/src/app/calendar/page.js
@@ -2,10 +2,30 @@
 import { useState } from 'react';
 import Calendar from 'react-calendar';
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import './Calendar.css';
 
 function CalendarComponent() {
+  const [value, onChange] = useState(new Date());
   return (
-    <Calendar/>
+    <div className="App">
+
+      {/* <header className="App-header">
+      <h2>The weather scheduling for you</h2>
+      <p>Experience the freedom of planning that adapts to the weather conditions, keeping you one step ahead.</p>
+      <button className="explore">Explore all features</button>
+      <button className="start">Get Started</button>
+      <div className="container">
+        <Image src="/image-3.png" alt="Calendar"  width={900} height={900} />
+      </div>
+    </header> */}
+
+      <Calendar onChange={onChange} value={value} />
+
+
+
+    </div>
   )
 }
 
